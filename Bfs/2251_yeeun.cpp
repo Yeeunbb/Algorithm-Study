2808a88cb74e43815de8ac00c50c0d7f5fe11f4d
YEEUN <ykoou@naver.com>
2021-01-20 00:26:05 +0900

    BOJ 2251 solution

--- Bfs/2251_yeeun.cpp
+++ Bfs/2251_yeeun.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 void bfs(int a, int b, int c);
-bool isValid(int a, int b);
+bool isValid(int a, int b, int r);
 int A, B, C;
 int visited[201][201][201];
-int c[201];
+int ofC[201];
 typedef struct{
 	int a;
 	int b;
@@ -17,81 +17,83 @@ int main(){
 	cin >> A >> B >> C;
 
 	bfs(0, 0, C);
+	for(int i=0; i<=200; i++){
+		if(ofC[i]==1){
+			cout << i << " ";
+		}
+	}
 	return 0;
 }
 
 void bfs(int a, int b, int c){
 	queue<bottle> bo;
 	bo.push({a,b,c});
-	visited[a][b][c] = 0;
-	
+
 	while(!bo.empty()){
 		bottle now = bo.front();
 		bo.pop();
+
 		int a = now.a;
 		int b = now.b;
 		int c = now.c;
-		
-		if(isValid(a,b)){
+
+		if(visited[a][b][c]==1) continue;
+		visited[a][b][c] = 1;
+		if(a==0) ofC[c] = 1;
+
+		if(isValid(a,b,B)){
 			if(a+b > B){
-				if(visited[a+b-B,B,c]!=1){
-					bo.push({a+b-B, B, c});
-					visited[a+b-B,B,c] = 1;
-				}
+				bo.push({a+b-B, B, c});
 			}
-			else{ 
-				if(visited[0, a+b, c]!=1){
-					bo.push({0, a+b, c});
-					visited[0,a+b,c] = 1;
-				}
+			else{
+				bo.push({0, a+b, c});
 			}
-			c[c] = 1;
 		}
-		if(isValid(a,c)){
+		if(isValid(a,c,C)){
 			if(a+c > C){
-				if(visited[a+c-C, b, C]!=1){
-					bo.push({a+c-C, b, C});
-					visited[a+c-C,b,C];
-					c[C]=1;
-				}
+				bo.push({a+c-C, b, C});
 			}
 			else{
-				if(visited[0,b,a+c]!=1){
-					bo.push({0,b,a+c});
-					visited[0,b,a+c];
-					c[a+c]=1;
-				}
+				bo.push({0,b,a+c});
 			}
 		}
-		if(isValid(b,a)){
+		if(isValid(b,a,A)){
 			if(b+a > A){
-				if(visited[A,b+a-A,c]!=1){
-					bo.push({A,b+a-A,c});
-					visited[A,b+a-A,c];
-				}
+				bo.push({A,b+a-A,c});
 			}
 			else{
-				if(visited[b+a, 0, c]!=1){
-					bo.push({b+a,0,c});
-					visited[b+a,0,c];
-				}
+				bo.push({b+a,0,c});
 			}
-			c[c]=1;			
 		}
-		if(isValid(b,c)){
-			
+		if(isValid(b,c,C)){
+			if(b+c > C){
+				bo.push({a,b+c-C,C});
+			}
+			else{
+				bo.push({a,0,b+c});
+			}
 		}
-		if(isValid(c,a)){
-			
+		if(isValid(c,a,A)){
+			if(c+a > A){
+				bo.push({A,b,c+a-A});
+			}
+			else{
+				bo.push({c+a,b,0});
+			}
 		}
-		if(isValid(c,b)){
-			
+		if(isValid(c,b,B)){
+			if(c+b > B){
+				bo.push({a, B, c+b-B});
+			}
+			else{
+				bo.push({a,c+b,0});
+			}
 		}
 	}
 }
 
-bool isValid(int a, int b){
+bool isValid(int a, int b, int r){
 	if(a<=0) return false;
-	if(b==B) return false;
+	if(b==r) return false;
 	return true;
 }
