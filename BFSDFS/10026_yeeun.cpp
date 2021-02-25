1f5666af453e516a939eb6c3ea62b7cb459cad1a
YEEUN <ykoou@naver.com>
2021-02-25 21:25:45 +0900

    BOJ 10026 Solution

---
+++ BFSDFS/10026_yeeun.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+int N;
+char map[100][100];
+int visited[100][100];
+int dir[4][2] = { {0, -1}, {0, 1}, {1, 0}, {-1, 0} };
+void bfs(int i, int j);
+void bfs2(int i, int j);
+
+int main() {
+	int cnt=0;
+
+	cin >> N;
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			cin >> map[i][j];
+		}
+	}
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(visited[i][j]==1) continue;
+			else{
+				bfs(i,j);
+				cnt++;
+			}
+		}
+	}
+	cout << cnt << " ";
+	cnt = 0;
+
+	memset(visited, 0, sizeof(visited));
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(visited[i][j]==1) continue;
+			else{
+				bfs2(i,j);
+				cnt++;
+			}
+		}
+	}
+	cout << cnt;
+	return 0;
+}
+
+void bfs(int i, int j){
+	queue<pair<int, int>> q;
+	q.push(make_pair(i,j));
+	visited[i][j] = 1;
+
+	while(!q.empty()){
+		int r = q.front().first;
+		int c = q.front().second;
+		q.pop();
+
+		for(int k=0; k<4; k++){
+			int nr = r + dir[k][0];
+			int nc = c + dir[k][1];
+
+			if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+			if(visited[nr][nc] == 1) continue;
+			else {
+				if(map[r][c] == map[nr][nc]){
+					q.push(make_pair(nr,nc));
+					visited[nr][nc] = 1;
+				}
+			}
+		}
+	}
+}
+
+void bfs2(int i, int j){
+	queue<pair<int, int>> q;
+	q.push(make_pair(i,j));
+	visited[i][j] = 1;
+
+	while(!q.empty()){
+		int r = q.front().first;
+		int c = q.front().second;
+		q.pop();
+
+		for(int k=0; k<4; k++){
+			int nr = r + dir[k][0];
+			int nc = c + dir[k][1];
+
+			if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+			if(visited[nr][nc] == 1) continue;
+			else {
+				if(map[r][c] == map[nr][nc]){
+					q.push(make_pair(nr,nc));
+					visited[nr][nc] = 1;
+				}
+				else if(map[r][c] == 'R' && map[nr][nc] == 'G'){
+					q.push(make_pair(nr,nc));
+					visited[nr][nc] = 1;
+				}
+				else if(map[r][c] == 'G' && map[nr][nc] == 'R'){
+					q.push(make_pair(nr,nc));
+					visited[nr][nc] = 1;
+				}
+			}
+		}
+	}
+}
+
